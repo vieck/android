dec0b9417974945275191b474302e9470ec5da07
Michael Vieck <mvieck@fusionalliance.com>
2024-02-28 19:07:13 -0500

    Compose Preview created for ChooseEntityView

--- wear/src/main/java/io/homeassistant/companion/android/views/ChooseEntityView.kt
+++ wear/src/main/java/io/homeassistant/companion/android/views/ChooseEntityView.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
@@ -16,6 +19,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.items
 import androidx.wear.compose.material3.Button
@@ -23,7 +27,6 @@ import androidx.wear.compose.material3.ButtonDefaults
 import androidx.wear.compose.material3.Text
 import com.mikepenz.iconics.compose.Image
 import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
-import io.homeassistant.companion.android.common.R as commonR
 import io.homeassistant.companion.android.common.data.integration.Entity
 import io.homeassistant.companion.android.common.data.integration.getIcon
 import io.homeassistant.companion.android.common.util.capitalize
@@ -31,7 +34,9 @@ import io.homeassistant.companion.android.data.SimplifiedEntity
 import io.homeassistant.companion.android.theme.WearAppTheme
 import io.homeassistant.companion.android.theme.getFilledTonalButtonColors
 import io.homeassistant.companion.android.util.stringForDomain
+import java.util.Calendar
 import java.util.Locale
+import io.homeassistant.companion.android.common.R as commonR
 
 @Composable
 fun ChooseEntityView(
@@ -150,3 +155,33 @@ private fun ChooseEntityChip(
         colors = getFilledTonalButtonColors()
     )
 }
+
+@PreviewLightDark
+@Composable
+fun ChooseEntityView_Empty_Preview() {
+    ChooseEntityView(entitiesByDomainOrder = remember {
+        mutableStateListOf()
+    }, entitiesByDomain = remember {
+        mutableStateMapOf()
+    }, favoriteEntityIds = remember { mutableStateOf(listOf()) }, onNoneClicked = {}, onEntitySelected = {}, allowNone = true)
+}
+
+@PreviewLightDark
+@Composable
+fun ChooseEntityView_WithData_Preview() {
+    ChooseEntityView(entitiesByDomainOrder = remember {
+        mutableStateListOf("first", "second")
+    }, entitiesByDomain = remember {
+        mutableStateMapOf(
+            Pair(
+                "first",
+                mutableStateListOf(Entity("first", "on", "", Calendar.getInstance(), Calendar.getInstance(), null))
+            ),
+            Pair(
+                "second",
+                mutableStateListOf(Entity("second", "on", "", Calendar.getInstance(), Calendar.getInstance(), null))
+            )
+        )
+    }, favoriteEntityIds = remember { mutableStateOf(listOf("first")) }, onNoneClicked = {}, onEntitySelected = {}, allowNone = false)
+}
+
